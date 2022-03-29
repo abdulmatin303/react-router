734eb40022fab6d69738739eb0b5740ca613ce20
amr303 <amr.abdulmatin@gmail.com>
2022-03-30 01:15:06 +0600

    many components create

---
+++ src/components/About/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h2>This is about us</h2>
+            <p>wana know about us?</p>
+            <p><small>Taile age biryani khawaile...</small></p>
+        </div>
+    );
+};
+
+export default About;
