ae80190aaa1af2bc3df71e89a9ba4ee82c8cef5f
amr303 <amr.abdulmatin@gmail.com>
2022-04-01 13:07:39 +0600

    load data based on dynamic url

--- src/components/FriendDetail/FriendDetail.js
+++ src/components/FriendDetail/FriendDetail.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const FriendDetail = () => {
     const {friendId}= useParams();
+    const [friend,setFriend]=useState({});
+
+    useEffect(()=> {
+        const url=`https://jsonplaceholder.typicode.com/users/${friendId}`
+        fetch(url)
+        .then(res=> res.json())
+        .then(data=> setFriend(data))
+    },[])
     return (
         <div>
             <h3>Friend Details of a bondhu {friendId} </h3>
+            <p>name: {friend.name}</p>
+            <p>email: {friend.email}</p>
+            <p>website: {friend.website}</p>
+            <p>address: {friend.address?.city}</p>
+
         </div>
     );
 };
